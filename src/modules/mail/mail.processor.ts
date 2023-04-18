662f926b1d365d49bf4a45f1dbfd26ededad7e9e
alethienvu <ale.thienvu@setel.com>
2023-04-18 11:21:07 +0700

    fix: updte

--- src/modules/mail/mail.processor.ts
+++ src/modules/mail/mail.processor.ts
@@ -1,12 +1,13 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Processor } from '@nestjs/bull';
 import { Logger } from '@nestjs/common';
-import { getConfig } from 'src/configs';
+// import { getConfig } from 'src/configs';
 import { UserService } from 'src/modules/user/users.service';
 
 @Processor('mail')
 export class MailProcessor {
-  public static MAIL_BANNER_LINK = `${getConfig().get<string>('mail.domain')}banner.png`;
+  public static MAIL_BANNER_LINK = `https://i.pinimg.com/originals/99/c8/1f/99c81f02680db118ce8f1580c2276823.jpg`;
+  // public static MAIL_BANNER_LINK = `${getConfig().get<string>('mail.domain')}banner.png`;
 
   constructor(
     private readonly mailerService: MailerService,
